bbc0c5be06f65a10ccc914f9395caeda16e09fd2
wndrms <wndrms2006@naver.com>
2023-02-22 14:42:21 +0900

    CSS 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import Button from "./components/button";
 import TextToJson from "./components/TextToJson";
 
 function App() {
-  const alarm = () => {
-    alert("Hi~");
-  };
   return (
     <div>
       <TextToJson></TextToJson>
--- src/components/TextToJson.tsx
+++ src/components/TextToJson.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './TextToJson.css';
 
 const TextToJson = () => {
     const [inputText, setInputText] = useState('');
+    const [isSaving, setIsSaving] = useState(false);
 
     const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         setInputText(event.target.value);
@@ -10,18 +12,25 @@ const TextToJson = () => {
 
     const handleSaveButtonClick = async () => {
         const fileData = JSON.stringify({ text: inputText });
+        setIsSaving(true);
         try {
             const response = await axios.post('/api/save-json-file', {data: fileData});
             console.log(response.data);
         } catch (error) {
             console.error(error);
+        } finally {
+            setIsSaving(false);
         }
     };
 
     return (
-        <div>
-            <textarea value={inputText} onChange={handleInputChange}/>
-            <button onClick={handleSaveButtonClick}>Save as JSON</button>
+        <div className='text-to-json-container'>
+            <div className='textarea-container'>
+                <textarea className='textarea' value={inputText} onChange={handleInputChange}/>
+            </div>     
+            <button className='button' onClick={handleSaveButtonClick} disabled={isSaving}>
+                {isSaving ? 'Saving...' : 'Save as JSON'}
+            </button>
         </div>
     )
 }
